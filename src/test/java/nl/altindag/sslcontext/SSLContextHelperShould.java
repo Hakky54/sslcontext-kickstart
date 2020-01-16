1fadbb6426aa8b27b22a2611b0daa5131c940989
Hakan Altindag <hakangoudberg@hotmail.com>
2020-01-16 22:23:38 +0100

    Improved exception message

--- src/test/java/nl/altindag/sslcontext/SSLContextHelperShould.java
+++ src/test/java/nl/altindag/sslcontext/SSLContextHelperShould.java
@@ -32,7 +32,7 @@ public class SSLContextHelperShould {
 
     private static final String GENERIC_IDENTITY_VALIDATION_EXCEPTION_MESSAGE = "Identity details are empty, which are required to be present when SSL/TLS is enabled";
     private static final String GENERIC_TRUSTSTORE_VALIDATION_EXCEPTION_MESSAGE = "TrustStore details are empty, which are required to be present when SSL/TLS is enabled";
-    private static final String GENERIC_TRUST_STRATEGY_VALIDATION_EXCEPTION_MESSAGE = "Trust strategy is missing. Please validate if the TrustStore is present, or default including the JDK trustStore is enabled or trusting all certificates without validation is enabled";
+    private static final String GENERIC_TRUST_STRATEGY_VALIDATION_EXCEPTION_MESSAGE = "Trust strategy is missing. Please validate if the TrustStore is present, or including default JDK trustStore is enabled or trusting all certificates without validation is enabled";
 
     private static final String IDENTITY_FILE_NAME = "identity.jks";
     private static final String TRUSTSTORE_FILE_NAME = "truststore.jks";
