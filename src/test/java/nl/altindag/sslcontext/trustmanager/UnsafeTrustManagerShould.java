2d680aa5306acc1c04db058320a8c3e646e01af0
Hakan Altindag <hakangoudberg@hotmail.com>
2020-02-10 21:23:57 +0100

    Added unit tests to validate test cases without debug level

--- src/test/java/nl/altindag/sslcontext/trustmanager/UnsafeTrustManagerShould.java
+++ src/test/java/nl/altindag/sslcontext/trustmanager/UnsafeTrustManagerShould.java
@@ -13,8 +13,10 @@ import java.security.cert.X509Certificate;
 import javax.net.ssl.X509TrustManager;
 
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import nl.altindag.log.LogCaptor;
 import nl.altindag.sslcontext.util.KeystoreUtils;
 
@@ -27,8 +29,10 @@ public class UnsafeTrustManagerShould {
     private static final String KEYSTORE_LOCATION = "keystores-for-unit-tests/";
 
     @Test
-    public void notThrowExceptionWhenValidatingCertificate() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+    public void checkClientTrusted() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
         LogCaptor logCaptor = LogCaptor.forClass(UnsafeTrustManager.class);
+
+        configureDebugLogging(true);
 
         KeyStore trustStore = KeystoreUtils.loadKeyStore(KEYSTORE_LOCATION + TRUSTSTORE_FILE_NAME, TRUSTSTORE_PASSWORD);
         X509TrustManager trustManager = UnsafeTrustManager.INSTANCE;
@@ -49,6 +53,8 @@ public class UnsafeTrustManagerShould {
     public void checkServerTrusted() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
         LogCaptor logCaptor = LogCaptor.forClass(UnsafeTrustManager.class);
 
+        configureDebugLogging(true);
+
         X509Certificate[] trustedCerts = KeyStoreTestUtils.getTrustedX509Certificates(KeystoreUtils.loadKeyStore(KEYSTORE_LOCATION + KEYSTORE_FILE_NAME, KEYSTORE_PASSWORD));
 
         X509TrustManager trustManager = UnsafeTrustManager.INSTANCE;
@@ -61,6 +67,54 @@ public class UnsafeTrustManagerShould {
 
         assertThat(logCaptor.getLogs(Level.DEBUG)).hasSize(1);
         assertThat(logCaptor.getLogs(Level.DEBUG)).contains("Accepting a server certificate: CN=Prof Oak, OU=Oak Pokémon Research Lab, O=Oak Pokémon Research Lab, C=Pallet Town");
+    }
+
+    @Test
+    public void checkClientTrustedDoesNotLogAnythingWhenDebugLevelIsDisabled() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        LogCaptor logCaptor = LogCaptor.forClass(UnsafeTrustManager.class);
+
+        configureDebugLogging(false);
+
+        KeyStore trustStore = KeystoreUtils.loadKeyStore(KEYSTORE_LOCATION + TRUSTSTORE_FILE_NAME, TRUSTSTORE_PASSWORD);
+        X509TrustManager trustManager = UnsafeTrustManager.INSTANCE;
+        X509Certificate[] trustedCerts = KeyStoreTestUtils.getTrustedX509Certificates(trustStore);
+
+        assertThat(trustManager).isNotNull();
+        assertThat(trustManager.getAcceptedIssuers()).hasSize(0);
+        assertThat(trustedCerts).hasSize(1);
+
+        assertThatCode(() -> trustManager.checkClientTrusted(trustedCerts, "RSA"))
+                .doesNotThrowAnyException();
+
+        assertThat(logCaptor.getLogs(Level.DEBUG)).isEmpty();
+    }
+
+    @Test
+    public void checkServerTrustedDoesNotLogAnythingWhenDebugLevelIsDisabled() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        LogCaptor logCaptor = LogCaptor.forClass(UnsafeTrustManager.class);
+
+        configureDebugLogging(false);
+
+        X509Certificate[] trustedCerts = KeyStoreTestUtils.getTrustedX509Certificates(KeystoreUtils.loadKeyStore(KEYSTORE_LOCATION + KEYSTORE_FILE_NAME, KEYSTORE_PASSWORD));
+
+        X509TrustManager trustManager = UnsafeTrustManager.INSTANCE;
+
+        assertThat(trustedCerts).hasSize(1);
+        assertThat(trustManager.getAcceptedIssuers()).hasSize(0);
+
+        assertThatCode(() -> trustManager.checkServerTrusted(trustedCerts, "RSA"))
+                .doesNotThrowAnyException();
+
+        assertThat(logCaptor.getLogs(Level.DEBUG)).isEmpty();
+    }
+
+    private void configureDebugLogging(boolean debugLoggingEnabled) {
+        Logger logger = (Logger) LoggerFactory.getLogger(UnsafeTrustManager.class.getName());
+        if (debugLoggingEnabled) {
+            logger.setLevel(Level.DEBUG);
+        } else {
+            logger.setLevel(Level.INFO);
+        }
     }
 
 }
