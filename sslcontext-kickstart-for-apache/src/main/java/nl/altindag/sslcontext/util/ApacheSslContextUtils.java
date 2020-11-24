709fd4df7258f37d6afce98aa13cfbcbe32ab1d7
Hakan Altindag <hakangoudberg@hotmail.com>
2020-11-24 10:59:59 +0100

    Made method name shorter

--- sslcontext-kickstart-for-apache/src/main/java/nl/altindag/sslcontext/util/ApacheSslContextUtils.java
+++ sslcontext-kickstart-for-apache/src/main/java/nl/altindag/sslcontext/util/ApacheSslContextUtils.java
@@ -8,7 +8,15 @@ public final class ApacheSslContextUtils {
 
     private ApacheSslContextUtils() {}
 
+    /**
+     * @deprecated Will be removed with version 6.0.0, please use {@link ApacheSslContextUtils#toSocketFactory(SSLFactory)}
+     */
+    @Deprecated
     public static LayeredConnectionSocketFactory toLayeredConnectionSocketFactory(SSLFactory sslFactory) {
+        return toSocketFactory(sslFactory);
+    }
+
+    public static LayeredConnectionSocketFactory toSocketFactory(SSLFactory sslFactory) {
         return new SSLConnectionSocketFactory(
                 sslFactory.getSslContext(),
                 sslFactory.getSslParameters().getProtocols(),
