ddc808347d0408574abcfb00dbe07d58fb410ef9
Hakan Altindag <hakangoudberg@hotmail.com>
2021-01-29 15:19:04 +0100

    Disabled netty verbose logging during integration test

--- sslcontext-kickstart-for-netty/src/test/java/nl/altindag/ssl/SSLFactoryIT.java
+++ sslcontext-kickstart-for-netty/src/test/java/nl/altindag/ssl/SSLFactoryIT.java
@@ -41,7 +41,9 @@ class SSLFactoryIT {
 
     @Test
     void executeHttpsRequestWithMutualAuthentication() throws IOException {
-        LogCaptor logCaptor = LogCaptor.forRoot();
+        LogCaptor.forName("io.netty").disableLogs();
+        LogCaptor.forName("reactor.netty").disableLogs();
+        LogCaptor logCaptor = LogCaptor.forName("nl.altindag.ssl");
 
         SSLFactory sslFactory = SSLFactory.builder()
                 .withIdentityMaterial("keystores-for-unit-tests/badssl-identity.p12", "badssl.com".toCharArray())
