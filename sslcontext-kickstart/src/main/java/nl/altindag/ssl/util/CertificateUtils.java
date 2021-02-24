9c6c50379ad2643a07e3a6fe1cfce247e0e3f628
Hakan Altindag <hakangoudberg@hotmail.com>
2021-02-25 00:07:07 +0100

    Replaced varargs with an array

--- sslcontext-kickstart/src/main/java/nl/altindag/ssl/util/CertificateUtils.java
+++ sslcontext-kickstart/src/main/java/nl/altindag/ssl/util/CertificateUtils.java
@@ -81,8 +81,7 @@ public final class CertificateUtils {
         return loadCertificate(Function.identity(), certificateStreams);
     }
 
-    @SafeVarargs
-    private static <T> List<Certificate> loadCertificate(Function<T, InputStream> resourceMapper, T... resources) {
+    private static <T> List<Certificate> loadCertificate(Function<T, InputStream> resourceMapper, T[] resources) {
         List<Certificate> certificates = new ArrayList<>();
         for (T resource : resources) {
             try (InputStream certificateStream = resourceMapper.apply(resource)) {
