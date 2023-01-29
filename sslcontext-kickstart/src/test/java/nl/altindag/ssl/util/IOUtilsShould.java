e445566ff8dda2d4785586500383015b0076c3db
Hakky54 <hakangoudberg@hotmail.com>
2023-01-29 16:36:25 +0100

    Removed unused import

--- sslcontext-kickstart/src/test/java/nl/altindag/ssl/util/IOUtilsShould.java
+++ sslcontext-kickstart/src/test/java/nl/altindag/ssl/util/IOUtilsShould.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.ByteArrayInputStream;
