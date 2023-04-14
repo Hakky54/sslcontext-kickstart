5e943aa05f58780f58c9a46f3397640d2a3136b0
em <eugen.mayer@kontextwork.de>
2023-04-14 19:21:55 +0200

    Add build in docker container and add CI task
    Use docker based builds to run builds in linux
     - debian
     - alpine
     - centos

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-BUILD_CMD=./mvnw clean verify --no-transfer-progress -Dlicense.skip=true
+set -e
 
-IMAGE={0:-bellsoft/liberica-openjdk-alpine:17}
+IMAGE=${1:-bellsoft/liberica-openjdk-alpine:17}
+BUILD_CMD=${2:-./mvnw clean verify --no-transfer-progress -Dlicense.skip=true}
+SETUP_CMD=$3
 
-echo "Using IMAGE> $IMAGE"
-docker run -v ./:/src -w /src $IMAGE $BUILD_CMD
+if [ -z "$var" ]; then
+  echo "No setup command provded, trying to auto-detect"
+  if [[ "$IMAGE" == *"debian"* ]]; then
+    echo "Using debian setup steps"
+    SETUP_CMD='apt-get update && apt-get install -y ca-certificates'
+  elif [[ "$IMAGE" == *"alpine"* ]]; then
+    echo "Using alpine setup steps"
+    SETUP_CMD='apk --update add ca-certificates'
+  elif [[ "$IMAGE" == *"centos"* ]]; then
+    echo "Using centos setup steps"
+    SETUP_CMD='yum clean all && yum install -y ca-certificates'
+  else
+    echo "unknown distro, skipping setup step"
+    SETUP_CMD=
+  fi
+fi
 
-#declare -a IMAGES=(bellsoft/liberica-openjdk-alpine:17 bellsoft/liberica-openjdk-debian:17)
-#for IMAGE in "${$IMAGES[@]}"
-#do
-#done
-
+echo "Using IMAGE: $IMAGE"
+docker run -v ./:/src -w /src $IMAGE sh -c "$SETUP_CMD && $BUILD_CMD"
 
 
