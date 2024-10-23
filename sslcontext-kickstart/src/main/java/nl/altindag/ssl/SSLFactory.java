68c4f8c8f39c91b61c072cacfbd7419f2b5cd18d
Hakky54 <hakangoudberg@hotmail.com>
2024-10-23 14:53:20 +0200

    Removed unused imports

--- sslcontext-kickstart/src/main/java/nl/altindag/ssl/SSLFactory.java
+++ sslcontext-kickstart/src/main/java/nl/altindag/ssl/SSLFactory.java
@@ -69,7 +69,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
